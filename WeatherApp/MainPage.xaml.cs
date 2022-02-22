f45f339d60a49304ec32a82def288f52e642520e
Lxoy <lo14loyt@gmail.com>
2022-02-22 18:14:48 +0100

    Updated MainPage.xaml.cs

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -5,27 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using Newtonsoft.Json;  
 using Xamarin.Forms;
 
 namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
-        string URL;
-        const string appid = "";
+        const string appid = "3b7c3947e8e22c86b32d822ad4c3a6b6";
         string cityName = "Samobor";
         public MainPage()
         {
             InitializeComponent();
-            GetCurrentWeather();
+            GetCurrentWeather(cityName);
         }
 
-        void GetCurrentWeather()
+        void GetCurrentWeather(string cityName)
         {
-            URL = "https://api.openweathermap.org/data/2.5/weather?q=Samobor&appid=3b7c3947e8e22c86b32d822ad4c3a6b6";
             using (WebClient web = new WebClient())
-            { 
-                var json = web.DownloadString(URL);
+            {
+                string url = String.Format("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + appid);
+                var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<CurrentWeatherInfo.root>(json);
+                CurrentWeatherInfo.root outPut = result;
             }        
         }
     }
